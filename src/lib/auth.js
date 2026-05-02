9139e631b41c2307e233103653e7340b5ecec033
sarwar-jahan1 <sarwarjahans2001@gamil.com>
2026-05-02 22:11:45 +0600

    Fix deployment problem

--- src/lib/auth.js
+++ src/lib/auth.js
@@ -1,20 +1,16 @@
 import { betterAuth } from "better-auth";
 import { MongoClient } from "mongodb";
+import { mongodbAdapter } from "better-auth/adapters/mongodb";
 
-const client = new MongoClient(process.env.MONGODB_URI);
-await client.connect();
+const client = new MongoClient("process.env.MONGODB_URI");
+const db = client.db("libraryhub");
 
 export const auth = betterAuth({
-  database: client.db("libraryhub"),
-
-  emailAndPassword: {
-    enabled: true,
-  },
-
-  socialProviders: {
-    google: {
-      clientId: process.env.GOOGLE_CLIENT_ID,
-      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-    },
-  },
+  database: mongodbAdapter(db, {
+    // Optional: if you don't provide a client, database transactions won't be enabled.
+    client
+  }),
+  emailAndPassword:{
+    enabled:true
+  }
 });
