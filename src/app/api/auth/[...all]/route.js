25344f22f9c9288b010d1b7580eda2501ead2d6d
sarwar-jahan1 <sarwarjahans2001@gamil.com>
2026-05-02 22:37:04 +0600

    Fix error

--- src/app/api/auth/[...all]/route.js
+++ src/app/api/auth/[...all]/route.js
@@ -1,4 +1,3 @@
-import { auth } from "@/lib/auth"; // path to your auth file
-import { toNextJsHandler } from "better-auth/next-js";
+import { auth } from "@/lib/auth";
 
-export const { POST, GET } = toNextJsHandler(auth);
+export const { GET, POST } = auth;
--- src/lib/auth.js
+++ src/lib/auth.js
@@ -2,15 +2,16 @@ import { betterAuth } from "better-auth";
 import { MongoClient } from "mongodb";
 import { mongodbAdapter } from "better-auth/adapters/mongodb";
 
-const client = new MongoClient("process.env.MONGODB_URI");
+const client = new MongoClient(process.env.MONGODB_URI); 
+await client.connect();
+
 const db = client.db("libraryhub");
 
 export const auth = betterAuth({
   database: mongodbAdapter(db, {
-    // Optional: if you don't provide a client, database transactions won't be enabled.
-    client
+    client,
   }),
-  emailAndPassword:{
-    enabled:true
-  }
+  emailAndPassword: {
+    enabled: true,
+  },
 });
